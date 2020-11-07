dc220fa609381e7a4dd5000e6eda521f0b9211e7
vineet <vineetsikri@yahoo.com>
2020-11-06 23:11:12 -0600

    final

--- 15-Interactive-Visualizations-and-Dashboards/Instructions/StarterCode/data/app.js
+++ 15-Interactive-Visualizations-and-Dashboards/Instructions/StarterCode/data/app.js
@@ -1,29 +1,29 @@
 //  d3.select("selDataset");
 //  button.on("change",runenter); 
 //  var button =
-d3.json("data/samples.json").then((sampleData) => {
-    console.log(sampleData);
-    var selectTag = d3.select("select");
+// d3.json("data/samples.json").then((sampleData) => {
+//     console.log(sampleData);
+//     var selectTag = d3.select("select");
 
-    //we have select all options tags from inside select tag (which there are 0 atm)
-    //and assigned data as to be the base of modelling that selection.
-    var options = selectTag.selectAll('option')
-        .data(sampleData.names);
+//     //we have select all options tags from inside select tag (which there are 0 atm)
+//     //and assigned data as to be the base of modelling that selection.
+//     var options = selectTag.selectAll('option')
+//         .data(sampleData.names);
        
 
-    //d3 sees we have less elements (0) than the data (2), so we are tasked to create
-    //these missing inside the `options.enter` pseudo selection.
-    //if we had some elements from before, they would be reused, and we could access their
-    //selection with just `options`
-    options.enter()
-        .append('option')
-        .attr('value', function (d) {
-            return d;
-        })
-        .text(function (d) {
-            return d;
-        });
-});
+//     //d3 sees we have less elements (0) than the data (2), so we are tasked to create
+//     //these missing inside the `options.enter` pseudo selection.
+//     //if we had some elements from before, they would be reused, and we could access their
+//     //selection with just `options`
+//     options.enter()
+//         .append('option')
+//         .attr('value', function (d) {
+//             return d;
+//         })
+//         .text(function (d) {
+//             return d;
+//         });
+// });
 
 
 
--- 15-Interactive-Visualizations-and-Dashboards/Instructions/StarterCode/data/plot.js
+++ 15-Interactive-Visualizations-and-Dashboards/Instructions/StarterCode/data/plot.js
@@ -1,89 +1,123 @@
 
 // use d3 json () to fetch data from json file
 // use function as optionChanged per html on chang
-function optionChanged(sampleID) {
+
 d3.json("data/samples.json").then((sampleData) => {
     console.log(sampleData);
-var metadata = sampleData.metadata;
-// // use filter () to pass the function as its argument 
-var filteredSamples = metadata.filter(x => x.id == sampleID);
+    var selectTag = d3.select("select");
 
-    var m = d3.select("#sample-metadata");
-    //make sure to empty html before appending and loading data
+    //we have select all options tags from inside select tag (which there are 0 atm)
+    //and assigned data as to be the base of modelling that selection.
+    var options = selectTag.selectAll('option')
+        .data(sampleData.names);
+
+
+    //d3 sees we have less elements (0) than the data (2), so we are tasked to create
+    //these missing inside the `options.enter` pseudo selection.
+    //if we had some elements from before, they would be reused, and we could access their
+    //selection with just `options`
+    options.enter()
+        .append('option')
+        .attr('value', function (d) {
+            return d;
+        })
+        .text(function (d) {
+            return d;
+        });
+    var firstSample = 940;
+    demodata(firstSample);
+    demochart(firstSample);
+});
+function optionChanged(sample){
+    demodata(sample);
+    demochart(sample);
+}
+
+function demodata(sample) {
+    d3.json("data/samples.json").then((sampleData) => {
+        console.log(sampleData);
+        var metadata = sampleData.metadata;
+        // // use filter () to pass the function as its argument 
+        var filteredSamples = metadata.filter(x => x.id == sample);
+
+        var m = d3.select("#sample-metadata");
+        //make sure to empty html before appending and loading data
         m.html("")
-        Object.entries(filteredSamples[0]).forEach(function([key,value]){
-            console.log(key,value);
-        
-             m.append("h3").text(value);
-            
+        Object.entries(filteredSamples[0]).forEach(function ([key, value]) {
+            console.log(key, value);
+
+            m.append("h3").text(value);
+
         });
     });
-
-  
-
-d3.json("data/samples.json").then((sampleData) => {
-    console.log(sampleData);
-    var samples = sampleData.samples;
-    // // use filter () to pass the function as its argument 
-    var filteredSample = samples.filter(y => y.otu_id === sampleID);
+}
+var demo;
+function demochart(sample) {
+    d3.json("data/samples.json").then((sampleData) => {
+        console.log(sampleData);
+        var sampleset = sampleData.samples;
+        console.log(sampleset);
+        var filteredSample = sampleset.filter(y => y.id == sample);
+        console.log(filteredSample,sample);
+        // var otu_ids = filteredSample.otu_ids;
+        // var sample_values = filteredSample.sample_values;
+        // var otu_labels = filteredSample.otu_labels;
     
-        var m1 = d3.select("#bar");
-        //make sure to empty html before appending and loading data
-            m1.html("")
-            // Object.entries(filteredSample[0]).forEach(function([key,value]){
-                // console.log(key,value);
-    var trace1 = {
-        x: sampleData.otu_ids,
-        y: sampleData.sample_values,
-        text: sampleData.otu_labels,
-        // name = "Ids",
-        type: "bar",
-        orientation: "h"
-    };
 
-    var samples = [trace1]
+        var otu_ids = filteredSample.map(sample => sample.otu_ids);
+        var sample_values = filteredSample.map(sample => sample.sample_values);
+        var otu_labels = filteredSample.map(sample => sample.otu_labels);
+        console.log(otu_labels);
+        var trace1 = {
+            x: otu_ids[0],
+            y: sample_values[0],
+            text: otu_labels[0],
+            // name = "Ids",
+            type: "bar",
+            orientation: "h"
+        };
 
-    var layout = {
-        title: "Belly_button_diversity",
-        xaxis: { title: "sample_values" },
-        yaxis: { title: "otu_ids" }
-    };
-    Plotly.newPlot("bar", samples, layout);
+        var samples = [trace1]
 
-    
+        var layout1 = {
+            title: "Belly_button_diversity",
+            xaxis: { title: "sample_values" },
+            yaxis: { title: "otu_ids" }
+        };
+
+        Plotly.newPlot("bar", samples, layout1)
+
+        // console.log(sampleData.otu_ids);
 
         // load data
-        var trace1 = {
-            x: sampleData.otu_ids,
-            y: sampleData.sample_value,
-            text: sampleData.otu_labels,
+        var trace2 = {
+            x: otu_ids[0],
+            y: sample_values[0],
+            text: otu_labels[0],
             mode: 'markers',
             marker: {
-              color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)',  'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
-              size: [40, 60, 80, 100]
+                color: ['rgb(93, 164, 214)', 'rgb(255, 144, 14)', 'rgb(44, 160, 101)', 'rgb(255, 65, 54)'],
+                size: [40, 60, 80, 100]
             }
-          };
-          
-          var data = [trace1];
-          
-          var layout = {
+        };
+
+        var data = [trace2];
+
+        var layout2 = {
             title: 'Bubble Chart Hover Text',
             showlegend: false,
             height: 600,
             width: 600
-          };
-          
-          Plotly.newPlot('bubble', data, layout);
-        
-      
-      });
-  
+        };
+
+        Plotly.newPlot('bubble', data, layout2);
+
+
+    });
 
 
 
-
+    // }); 
 
 
 }
-
-optionChanged(940);
